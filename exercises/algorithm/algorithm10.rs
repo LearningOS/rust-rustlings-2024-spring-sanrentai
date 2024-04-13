266d34ef0a2924e4cf0b5bef650bbc9b6b91f3d4
Sany <sanrentai@163.com>
2024-04-13 05:54:30 +0000

    完成了

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -2,7 +2,7 @@
 	graph
 	This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
+
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -30,6 +30,19 @@ impl Graph for UndirectedGraph {
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (source, destination, weight) = edge;
+        
+        // 将边添加到源顶点的邻接表中
+        self.adjacency_table
+            .entry(source.to_string())
+            .or_insert_with(Vec::new)
+            .push((destination.to_string(), weight));
+        
+        // 因为是无向图，所以还要将边添加到目标顶点的邻接表中
+        self.adjacency_table
+            .entry(destination.to_string())
+            .or_insert_with(Vec::new)
+            .push((source.to_string(), weight));
     }
 }
 pub trait Graph {
@@ -38,10 +51,21 @@ pub trait Graph {
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
         //TODO
-		true
+        // 是否存在
+		if self.contains(node) {
+            false 
+        } else {
+            self.adjacency_table_mutable().insert(node.to_string(), Vec::new());
+            true 
+        }
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        let (node1, node2, weight) = edge;
+        // Add edge from node1 to node2
+        self.adjacency_table_mutable().entry(node1.to_string()).or_insert(Vec::new()).push((node2.to_string(), weight));
+        // Add edge from node2 to node1
+        self.adjacency_table_mutable().entry(node2.to_string()).or_insert(Vec::new()).push((node1.to_string(), weight));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
@@ -78,6 +102,7 @@ mod test_undirected_graph {
             (&String::from("c"), &String::from("b"), 10),
         ];
         for edge in expected_edges.iter() {
+            println!("{}",1);
             assert_eq!(graph.edges().contains(edge), true);
         }
     }
